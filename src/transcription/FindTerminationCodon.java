5cddc828a850613300f3796286572858f88739c2
Jae Min Kim <jmk1806@korea.ac.kr>
2018-11-20 02:02:58 +0900

    Rename_Termination

--- src/transcription/FindTerminationCodon.java
+++ src/transcription/FindTerminationCodon.java
@@ -1,7 +1,7 @@
 package transcription;
 
-public class FindFinishCodon {
-	FindFinishCodon(String [] token){
+public class FindTerminationCodon {
+	FindTerminationCodon(String [] token){
 		for(int i = 0; i < token.length; i++) {
 			if(token.equals("UAA")||token.equals("UAG")||token.equals("UGA")) whereEnd = i;
 			else whereEnd = -1;
