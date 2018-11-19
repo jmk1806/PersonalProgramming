490ab9504ed2d6dd28bdd74517397344db9be545
Jae Min Kim <jmk1806@korea.ac.kr>
2018-11-20 01:58:03 +0900

    FindInitiationCodon

--- src/transcription/FindStartCodon.java
+++ src/transcription/FindStartCodon.java
@@ -3,7 +3,7 @@ package transcription;
 public class FindStartCodon {
 	FindStartCodon(String [] token){
 		for(int i = 0; i < token.length; i++) {
-			if(token.equals("AUG")) whereStart = i;
+			if(token.equals("AUG")) whereStart = i; // 개시코돈 : AUG 찾기
 			else whereStart = -1;
 		}
 	}
