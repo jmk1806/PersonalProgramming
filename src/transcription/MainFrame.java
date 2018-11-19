910483bdd148d4c0ecf2c5790b09044ca42f411e
Jae Min Kim <jmk1806@korea.ac.kr>
2018-11-20 01:22:08 +0900

    Basic GUI Commit

--- src/transcription/MainFrame.java
+++ src/transcription/MainFrame.java
@@ -1,10 +1,19 @@
 package transcription;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class MainFrame {
-
+public class MainFrame extends JFrame{
+	MainFrame(){
+		setTitle("My Own Transcription Helper Program");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800,600);
+		setFocusable(true);
+		requestFocus();
+		setVisible(true);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MainFrame();
 	}
 
 }
